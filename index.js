81297253acd191e6ae54d40b09573ecb654d5db1
pele1610 <pelecolo56@gmail.com>
2026-04-13 12:58:37 +0300

    add async/await fetch and displayPosts function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+    return posts;
+}
+
+function displayPosts(posts) {
+    const postList = document.getElementById("post-list");
+
+    posts.forEach(function(post) {
+        const li = document.createElement("li");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = post.title;
+
+        const p = document.createElement("p");
+        p.textContent = post.body;
+
+        li.append(h1, p);
+        postList.append(li);
+    });
+}
+
+getPosts();
